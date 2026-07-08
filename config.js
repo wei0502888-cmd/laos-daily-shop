4979f2bbf830712be242bfbac6900078914d230d
wei0502888-cmd <wei0502888@gmail.com>
2026-07-08 13:58:54 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ window.SHOP_CONFIG = {
   currencyLabel: "$",
   telegram: {
     mode: "proxy",
-    orderEndpoint: "https://script.google.com/macros/s/AKfycbyx2uZjolUlUIbLi1nsiSI8Kqksmf4Ihgku6wwGE6AzSaOLt53NArPPneIMk9hJpGaJ/exec",
+    orderEndpoint: "https://script.google.com/macros/s/AKfycbxjV2WdkWufduFaKSTFS30ubUVsw9UaIkvcG44PdOg_Kt5uPDM37eSBduwNMLqR2qt5Xg/exec",
   },
-  orderStatusEndpoint: "https://script.google.com/macros/s/AKfycbyx2uZjolUlUIbLi1nsiSI8Kqksmf4Ihgku6wwGE6AzSaOLt53NArPPneIMk9hJpGaJ/exec",
+  orderStatusEndpoint: "https://script.google.com/macros/s/AKfycbxjV2WdkWufduFaKSTFS30ubUVsw9UaIkvcG44PdOg_Kt5uPDM37eSBduwNMLqR2qt5Xg/exec",
   payment: {
     title: "匯款 / U 帳號",
     account: "請洽專人提供 U 帳號",
