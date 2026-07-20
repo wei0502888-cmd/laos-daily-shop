d0976d9d4e40ada692e86e96d4e65e76027a6af1
wei0502888-cmd <wei0502888@gmail.com>
2026-07-20 21:27:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const config = window.SHOP_CONFIG || {
   telegram: { mode: "proxy", orderEndpoint: "" },
 };
 
-const BUILD_VERSION = "20260709-1";
+const BUILD_VERSION = "20260720-1";
 const IMAGE_PATH_PREFIXES = ["", "./", "老撾商城_商品圖正式導入版_0707/"];
 
 const iconMap = {
